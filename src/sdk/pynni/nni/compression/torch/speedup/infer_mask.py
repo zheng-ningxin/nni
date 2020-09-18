8b12eb99f7df79e538341f476f5aaacf7162b037
Ningxin <Ningxin.Zheng@microsoft.com>
2020-09-18 06:20:22 +0000

    updated

--- src/sdk/pynni/nni/compression/torch/speedup/infer_mask.py
+++ src/sdk/pynni/nni/compression/torch/speedup/infer_mask.py
@@ -1,9 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import logging
+import numpy as np
 import torch
 import torch.nn as nn
 
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
 
 class AutoMaskInference:
     """
@@ -28,8 +32,13 @@ class AutoMaskInference:
         """
         assert isinstance(module, torch.nn.Module)
         self.module = module
-        self.dummy_input = dummy_input
-        self.in_masks = in_masks
+        if isinstance(dummy_input, list):
+            assert isinstance(in_masks, list)
+            self.dummy_input = dummy_input
+            self.in_masks = in_masks
+        else:
+            self.dummy_input = [dummy_input]
+            self.in_masks = [in_masks]
         self.weight_mask = weight_mask
         self.output_mask = output_mask
         self.weights = {}
@@ -37,8 +46,16 @@ class AutoMaskInference:
         for name, para in module.named_parameters():
             self.weights[name] = para.data
 
-    def _random_init(self):
-        pass
+    def _random_init(self, start=1, end=10):
+        """
+        Random initialize the weights of the module. The value of
+        the tensor will not affect the mask auto inference.
+        """
+        for tensor in self.dummy_input:
+            if isinstance(tensor, torch.Tensor):
+                nn.init.uniform_(tensor.data, start, end)
+        for _, para in self.module.named_parameters():
+            nn.init.uniform_(para.data, start, end)
 
     def _zero_grad(self):
         # set the weight's gradient to zero
@@ -47,6 +64,14 @@ class AutoMaskInference:
         for tensor in self.dummy_input:
             if isinstance(tensor, torch.Tensor):
                 tensor.data.zero_()
+
+    def _apply_mask(self):
+        for tid, in_tensor in enumerate(self.dummy_input):
+            if isinstance(in_tensor, torch.Tensor) and self.in_masks[tid] is not None:
+                in_tensor.data *= self.in_masks[tid]
+        for name, para in self.module.named_parameters():
+            if name in self.weight_mask:
+                para.data *= self.weight_mask[name].data
 
     def _inputs_internal(self, target_tensor, target_mask):
         """
@@ -65,14 +90,14 @@ class AutoMaskInference:
         ----------
         target_tensor: tensor
             We try to infer the mask for the target_tensor.
-        input_tensors: list
-            The input tensors other than the target tensor.
-        input_masks: list
-            The corresponding mask of the other input tensors.
+        target_mask: tensor
+            The mask of the target_tensor
         """
         # we need to trace the mask relationship by the gradient
         self.module.train()
         self._zero_grad()
+        # enable the grad for the target tensor
+        target_tensor.requires_grad_()
         assert isinstance(target_tensor, torch.Tensor)
         tmp_out = self.module(*self.dummy_input)
         loss = torch.sum(tmp_out)
@@ -87,15 +112,16 @@ class AutoMaskInference:
                     tensor.data = 1
             for para_name in self.weights:
                 if para_name in self.weight_mask:
-                    self.weights[para_name].copy_(self.weight_mask[para_name].data)
+                    self.weights[para_name].copy_(
+                        self.weight_mask[para_name].data)
         loss.backward()
         # now all the positions that the gradient equals to zeros
         # can be masked in the target tensor
         grad = target_tensor.grad.data
-        target_mask.data[grad==0] = 0
+        target_mask.data[grad == 0] = 0
         return target_mask
 
-    def _backwards_nan(self, out_tensor, out_mask, in_tensor):
+    def _backwards_nan(self, out_tensor, out_mask, in_tensor, in_mask=None):
         """
         Find out which parts of the input are only used to calculate the part of the
         output that is masked out.
@@ -107,6 +133,9 @@ class AutoMaskInference:
             The mask tensor of the output tensor. out_mask only contains 0s and 1s, and
             0 indicate the corresponding position of the output tensor is pruned.
         in_tensor: torch.Tensor
+            The target tensor to infer the mask for.
+        in_mask: torch.Tensor
+            The mask of the target input tensor.
         Returns
         -------
         """
@@ -115,35 +144,63 @@ class AutoMaskInference:
         nan_mask = out_mask.clone().detach()
         nan_mask[nan_mask == 0] = float('nan')
         n_dim = len(in_tensor.size())
-        in_mask = torch.ones_like(in_tensor)
+        if in_mask is None:
+            in_mask = torch.ones_like(in_tensor)
+        _ori_in = in_tensor.clone().detach()
         flag = False
-        for _dim in range(n_dim):
-            if flag:
-                # We search the dim from 0, if the high-dimension
-                # already meets the requirements, then we can just
-                # skip the following dimensions
-                break
-            # try to mask as much positions as possible
-            # only if the out_put tensor is equal to nan_mask * out_tensor
-            for _index in range(in_tensor.size(_dim)):
-                # build the slice object for the tensor for
-                # corresponding dimension and position.
-                _slice = [slice(None, None, None) for i in range(_dim)]
-                _slice[_dim] = _index
-                _slice = tuple(_slice)
-                tmp_in_tensor = in_tensor.clone().detach()
-                # set part of the input tensor to be nan, if the new output
-                # tensor is same with out_mask * out_tensor, then these
-                # masked values in the input tesor are only used to calculate
-                # the values masked in the output tensor, which means it's safe
-                # to mask these values in the input tensor
-                tmp_in_tensor[_slice] = float('nan')
-                tmp_out_tensor = self.module(tmp_in_tensor)
-                if torch.equal(tmp_out_tensor, out_mask * out_tensor):
-                    # we can mask these values for the input tensor
-                    in_mask[_slice] = 0
-                    flag = True
+        with torch.no_grad():
+            for _dim in range(n_dim):
+                if flag:
+                    # We search the dim from 0, if the high-dimension
+                    # already meets the requirements, then we can just
+                    # skip the following dimensions
+                    break
+                # try to mask as much positions as possible
+                # only if the out_put tensor is equal to nan_mask * out_tensor
+                for _index in range(in_tensor.size(_dim)):
+                    # build the slice object for the tensor for
+                    # corresponding dimension and position.
+                    _slice = [slice(None, None, None) for i in range(n_dim)]
+                    _slice[_dim] = _index
+                    _slice = tuple(_slice)
+                    in_tensor.data.copy_(_ori_in)
+                    # set part of the input tensor to be nan, if the new output
+                    # tensor is same with out_mask * out_tensor, then these
+                    # masked values in the input tesor are only used to calculate
+                    # the values masked in the output tensor, which means it's safe
+                    # to mask these values in the input tensor
+                    in_tensor.data[_slice] = float('nan')
+                    tmp_out_tensor = self.module(*self.dummy_input)
+                    _logger.debug('Try the dimension: %d, Index: %d', _dim, _index)
+                    _logger.debug('The output tensor after nan colored: \n%s', str(tmp_out_tensor))
+                    _logger.debug('The expected output tensor: \n%s', str(nan_mask*out_tensor))
+                    _nan_pos1 = nan_mask.cpu().numpy()
+                    _nan_pos2 = tmp_out_tensor.cpu().numpy()
+                    if np.array_equal(np.isnan(_nan_pos1), np.isnan(_nan_pos2)):
+                        # if the position of the nans is exactly the same
+                        # we can mask these values for the input tensor
+                        _logger.info('Masking the %s', str(_slice))
+                        in_mask[_slice] = 0
+                        flag = True
         return in_mask
+
+    def _forwards_outmask(self, out_mask):
+        """
+        Infer the output mask for the output tensor.
+        """
+        # random initialize the input_tensors and the weights tensors
+        self._random_init()
+        # get the origin output tensor
+        _ori_out = self.module(*self.dummy_input)
+        # apply the mask for the input tensor and the weight tensor
+        self._apply_mask()
+        _new_out = self.module(*self.dummy_input)
+        _new_zeors = _new_out == 0
+        _ori_zeros = _ori_out == 0
+        # only mask the newly added zeros
+        _new_zeors[_ori_zeros] = False
+        out_mask[_new_zeors] = 0
+        return out_mask
 
 
 class MaskFunction:
